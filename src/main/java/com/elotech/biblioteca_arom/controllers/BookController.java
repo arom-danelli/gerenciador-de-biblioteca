3239d5b48773e69da6972a38a728c22a11f7fc86
arom-danelli <aromdanelli@hotmail.com>
2024-09-11 00:54:58 -0300

    fix google api

--- src/main/java/com/elotech/biblioteca_arom/controllers/BookController.java
+++ src/main/java/com/elotech/biblioteca_arom/controllers/BookController.java
@@ -2,7 +2,6 @@ package com.elotech.biblioteca_arom.controllers;
 
 import com.elotech.biblioteca_arom.entities.Book;
 import com.elotech.biblioteca_arom.services.BookService;
-import com.elotech.biblioteca_arom.services.GoogleBooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +19,6 @@ public class BookController {
 
     private final BookService bookService;
 
-    private final GoogleBooksService googleBooksService;
 
     /**
      * Construtor que injeta o serviço de gerenciamento de livros.
@@ -28,9 +26,8 @@ public class BookController {
      * @param bookService o serviço de livros
      */
     @Autowired
-    public BookController(BookService bookService, GoogleBooksService googleBooksService) {
+    public BookController(BookService bookService) {
         this.bookService = bookService;
-        this.googleBooksService = googleBooksService;
     }
 
     /**
@@ -67,20 +64,6 @@ public class BookController {
         return ResponseEntity.ok(book);
     }
 
-    /**
-     * Endpoint para buscar livros na API do Google Books com base no termo de busca fornecido.
-     * O termo de busca será utilizado para fazer uma requisição à API do Google Books,
-     * e os resultados encontrados serão retornados como uma lista de objetos Book.
-     *
-     * @param query o termo de busca a ser utilizado para procurar livros (exemplo: título, autor, etc.)
-     * @return ResponseEntity contendo uma lista de objetos Book correspondentes ao termo de busca
-     *         e o status HTTP 200 (OK) se a busca for bem-sucedida.
-     */
-    @GetMapping("/api/books/search")
-    public ResponseEntity<List<Book>> searchBooks(@RequestParam String query) {
-        List<Book> books = googleBooksService.searchBooks(query);
-        return ResponseEntity.ok(books);
-    }
     /**
      * Atualiza um livro existente com base no ID fornecido.
      *
